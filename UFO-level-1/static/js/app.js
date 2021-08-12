f575178e30ef3cf8bae36646c6e3cba3dd4fb4c8
Antler_Zutter <yycyyc.happy@gmail.com>
2021-08-12 15:58:47 +1000

    add table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,20 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+// select the table area with tbody
+var tbody = d3.select("tbody");
+
+// build a function to show the table data on index page
+function buildTable(data){
+    data.forEach((dataRow) => {
+        const row = tbody.append("tr");
+
+        Object.values(dataRow).forEach((val) => {
+            row.append("td").text(val);
+        }
+        );
+    });
+}
+
+buildTable(tableData)
